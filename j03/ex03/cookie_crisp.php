11164028f33ff5ca5119bc30c276e196a295411f
hikkary <z.kerkeb@gmail.com>
2016-04-07 23:36:54 +0200

    Selon vos désirs

--- j03/ex03/cookie_crisp.php
+++ j03/ex03/cookie_crisp.php
@@ -1,22 +1,11 @@
 <?php
-if (isset($_GET))
+if ($_GET['action'] !== NULL)
 {
-$arrow = array();
-$arrow = $_GET;
-
-if ($arrow[action] === "set")
-	setcookie($arrow[name], $arrow[value]);
-
-/*
-foreach ($arrow as $elem => $key) 
-{
-	if ($elem === "action" ) 
-	{
-		if ($key === "set")
-			echo "string";
-	}
-	echo $elem.": ";
-	echo $key."\n";
-}*/
+	if ($_GET['action'] == "set")
+		setcookie($_GET[name], $_GET[value], time() +3600);
+	else if ($_GET['action'] == "get" && $_COOKIE[$_GET['name']])
+		echo $_COOKIE[$_GET['name']]. "\n";
+	else if ($_GET['action'] == "del")
+		setcookie($_GET[name], $_GET[value], time() -3600);
 }
 ?>
