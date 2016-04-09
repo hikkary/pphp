67503f34ee7fe9c19dec5fd0922b6644f9a8f914
hikkary <z.kerkeb@gmail.com>
2016-04-09 14:46:03 +0200

    Ah oui ! sa marche, on aime sa

--- j04/ex01/create.php
+++ j04/ex01/create.php
@@ -1,33 +1,39 @@
 <?php
+if($_POST['login'] == "" | $_POST['passwd'] == "")
+{
+	echo "ERROR";
+	return;
+}
 if ($_POST['submit'] === 'OK')
 {
-	/*$_POST['passwd'] = hash("whirlpool",$_POST['passwd']);*/
-	$serie = serialize($_POST);
-
+	$_POST['passwd'] = hash("whirlpool",$_POST['passwd']);
+	$serie = $_POST;
 	if (file_exists('../private/passwd'))
 	{
+		$tab = array('login' => $_POST['login'] , 'passwd' => $_POST['passwd']);
 		$check = file_get_contents('../private/passwd');
-		echo $check;
 		$check2 = unserialize($check);
 		print_r($check2);
-		 foreach ($check2 as $c2 => $key) 
+		foreach ($check2 as $c2) 
 		 {
-		 		// if ($_POST['login'] === $check2['login'])
-		 		// {
-		 		// 	echo 'ERROR';
-		 		// 	return;
-		 		// }
+		 		echo $tab['login']."\n";
+		 		echo $c2['login']."\n";
+			 	if ($tab['login'] === $c2['login'])
+			 	{
+			 		echo 'ERROR';
+			 		return;
+			 	}
+		
 		 }
-		 $check = $check;	 
+		 $check2[] = $tab;
+		 $check = serialize($check2);
 		 file_put_contents('../private/passwd', $check);
 		 return;
 	}
 	if (file_exists('../private') === FALSE)
 		mkdir("../private");
-	if (file_exists('../private/passwd'))
-		$stock = file_get_contents('../private/passwd');
-	file_put_contents('../private/passwd', $serie);
-	$file = file_get_contents('../private/passwd');
+	$tab[] = array('login' => $_POST['login'] , 'passwd' => $_POST['passwd']);
+	file_put_contents('../private/passwd', serialize($tab));
 }
 else
 {
