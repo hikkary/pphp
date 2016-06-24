b19187f86f7dcb27176dc33ffdf7031ca0baca95
hikkary <z.kerkeb@gmail.com>
2016-06-24 20:49:46 +0200

    liste a l'enver ok . implementer suppression

--- j09/ex02/todo.js
+++ j09/ex02/todo.js
@@ -10,13 +10,41 @@ function add() {
 	 //  lien = "<a href='#'>";
 	 // fin_lien = "</a>";
 	message = prompt('Please enter a note');
-	
+	if(!message)
+		return;
 	// message = lien + message + fin_lien;
 
 	list = document.getElementById('ft_list');
-	div = document.createElement("div");
+	div = document.createElement("LI");
 	message = document.createTextNode(message);
 	div.appendChild(message);
-	list.appendChild(div);
+	div.style.listStyleType = "circle";
+	div.style.marginLeft = "25px";
+	div.style.fontFamily = "Times New Roman, Times, serif";
+	div.style.fontSize = "20px";
+	div.style.fontStyle = "bold";
+
+	// list.appendChild(div);
+	list.insertBefore(div,list.childNodes[0]);
 	console.log(list);
 }
+		
+	function del()
+	{
+		var list2 = document.getElementById("LI");//.getElementByTagName("LI");
+		console.log(list2);
+			for(li in list2)
+			{
+				list2[li].onclick=function(){
+					remove(this);
+				}
+			}
+	}
+
+
+	setInterval("del()", 300); 
+
+// function delete()
+// {
+	
+// }
